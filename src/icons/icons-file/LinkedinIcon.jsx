bbbd506a69fdd2f3236d8f1bab7012462b524724
victorio <vortegar28@gmail.com>
2024-03-08 21:57:29 -0300

    update icons

---
+++ src/icons/icons-file/LinkedinIcon.jsx
@@ -0,0 +1,40 @@
+
+export const LinkedinIcon = ({helpertext}) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 256 256"
+      width="50px"
+      height="50px"
+    >
+      <g
+        fill="#6c757d"
+        fillRule="nonzero"
+        stroke="none"
+        strokeWidth="1"
+        strokeLinecap="butt"
+        strokeLinejoin="miter"
+        strokeMiterlimit="10"
+        strokeDasharray=""
+        strokeDashoffset="0"
+        fontFamily="none"
+        fontWeight="none"
+        fontSize="none"
+        textAnchor="none"
+        style={{ mixBlendMode: 'normal' }}
+      >
+        <g transform="scale(10.66667,10.66667)">
+        <title>{helpertext}</title>
+          <style>
+            {`
+              path:hover {
+                fill: #ffffff;
+              }
+            `}
+          </style>
+          <path d="M19,3h-14c-1.105,0 -2,0.895 -2,2v14c0,1.105 0.895,2 2,2h14c1.105,0 2,-0.895 2,-2v-14c0,-1.105 -0.895,-2 -2,-2zM7.738,17v0c-0.697,0 -1.262,-0.565 -1.262,-1.262v-4.477c0.001,-0.696 0.566,-1.261 1.262,-1.261v0c0.697,0 1.262,0.565 1.262,1.262v4.477c0,0.696 -0.565,1.261 -1.262,1.261zM7.694,8.717c-0.771,0 -1.286,-0.514 -1.286,-1.2c0,-0.686 0.514,-1.2 1.371,-1.2c0.771,0 1.286,0.514 1.286,1.2c0,0.686 -0.514,1.2 -1.371,1.2zM16.779,17v0c-0.674,0 -1.221,-0.547 -1.221,-1.221v-2.605c0,-1.058 -0.651,-1.174 -0.895,-1.174c-0.244,0 -1.058,0.035 -1.058,1.174v2.605c0,0.674 -0.547,1.221 -1.221,1.221h-0.081c-0.674,0 -1.221,-0.547 -1.221,-1.221v-4.517c0,-0.697 0.565,-1.262 1.262,-1.262v0c0.697,0 1.262,0.565 1.262,1.262c0,0 0.282,-1.262 2.198,-1.262c1.219,0 2.196,0.977 2.196,3.174v2.605c0,0.674 -0.547,1.221 -1.221,1.221z"></path>
+        </g>
+      </g>
+    </svg>
+  );
+};
---
+++ src/icons/icons-file/WhatsappIcon.jsx
@@ -0,0 +1,42 @@
+
+export const WhatsappIcon = ({helpertext}) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      xmlns:xlink="http://www.w3.org/1999/xlink"
+      viewBox="0 0 256 256"
+      width="50px"
+      height="50px"
+    >
+      <g
+        fill="#6c757d"
+        fillRule="nonzero"
+        stroke="none"
+        strokeWidth="1"
+        strokeLinecap="butt"
+        strokeLinejoin="miter"
+        strokeMiterlimit="10"
+        strokeDasharray=""
+        strokeDashoffset="0"
+        fontFamily="none"
+        fontWeight="none"
+        fontSize="none"
+        textAnchor="none"
+        style={{ mixBlendMode: 'normal' }}
+      >
+        <g transform="scale(10.66667,10.66667)">
+        <title>{helpertext}</title>
+          <style>
+            {`
+              path:hover {
+                fill: #ffffff;
+              }
+            `}
+          </style>
+          <path d="M19.1,4.9c-2.1,-2.1 -4.9,-3.1 -7.9,-2.9c-4,0.3 -7.5,3.1 -8.7,7c-0.8,2.7 -0.5,5.6 0.9,8l-1.3,4.3c-0.1,0.4 0.3,0.8 0.7,0.7l4.5,-1.2c1.4,0.8 3,1.2 4.7,1.2v0c4.2,0 8.1,-2.6 9.4,-6.5c1.3,-3.9 0.4,-7.9 -2.3,-10.6zM16.9,15.6c-0.2,0.6 -1.2,1.1 -1.7,1.2c-0.5,0 -0.9,0.2 -3,-0.6c-2.5,-1 -4.1,-3.6 -4.3,-3.8c-0.1,-0.2 -1,-1.4 -1,-2.6c0,-1.2 0.6,-1.8 0.9,-2.1c0.2,-0.3 0.5,-0.3 0.7,-0.3c0.2,0 0.3,0 0.5,0c0.2,0 0.4,0 0.6,0.4c0.2,0.5 0.7,1.7 0.8,1.9c0.1,0.1 0.1,0.3 0,0.4c-0.1,0.2 -0.1,0.3 -0.2,0.4c-0.1,0.1 -0.3,0.3 -0.4,0.4c-0.1,0.1 -0.3,0.3 -0.1,0.5c0.1,0.3 0.6,1.1 1.4,1.7c1,0.8 1.8,1.1 2,1.2c0.3,0.1 0.4,0.1 0.5,-0.1c0.1,-0.2 0.6,-0.7 0.8,-1c0.2,-0.3 0.3,-0.2 0.6,-0.1c0.3,0.1 1.5,0.7 1.7,0.8c0.3,0.1 0.4,0.2 0.5,0.3c-0.1,0.3 -0.1,0.8 -0.3,1.4z"></path>
+        </g>
+      </g>
+    </svg>
+  );
+};
+
